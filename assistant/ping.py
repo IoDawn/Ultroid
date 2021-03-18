4c93e23bc26ad43d2a4d60ff564943354c181de0
IoDawn <75518008+IoDawn@users.noreply.github.com>
2021-03-19 06:42:12 +0700

    Update ping.py

--- assistant/ping.py
+++ assistant/ping.py
@@ -16,5 +16,5 @@ async def _(event):
     ms = (end - start).microseconds / 1000
     await asst.send_message(
         event.chat_id,
-        f"**🏓Pong!!**\n `{ms}ms`",
+        f"**🏓Pong!!**\n `📶{ms}ms`",
     )
